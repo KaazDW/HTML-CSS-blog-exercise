19d0456a215b6ef308378f833dec928a16ccd3ec
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-11-21 20:19:33 +0100

    tp1 done + start tp2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header");
-    // header.classList.toggle("sticky", window.scrollY > 10);
-})
+// window.addEventListener("scroll", function(){
+//     var header = document.querySelector("header");
+//     // header.classList.toggle("sticky", window.scrollY > 10);
+// })
+
+var nav = document.getElementById("nav");
+act = false;
+
+function mobilemenu() {
+    if (act === false){
+        nav.classList.remove("nonactive");
+        nav.classList.add("active");
+        act = true;
+        console.log("menu : " + act);
+    } else{
+        nav.classList.remove("active");
+        nav.classList.add("nonactive");
+        act = false;
+        console.log("menu : " + act);
+    }
+}
