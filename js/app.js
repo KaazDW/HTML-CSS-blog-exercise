0cc45061469dd33cab2d5e53997b2cbe78b3ee4f
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-11-23 12:19:20 +0100

    add failed js function

--- js/app.js
+++ js/app.js
@@ -15,6 +15,27 @@ function mobilemenu() {
         console.log("menu : " + act);
     }
 }
+
+
+var headtitle = document.getElementById("title");
+headtitle.innerHTML = "Hello";
+
+setTimeout(() => {
+    headtitle.innerHTML = "Hello";
+}, "1000")
+
+
+for(let i=10; i<0; i--){
+    headtitle.innerHTML = "Good Bye";
+    setTimeout(() => {
+        headtitle.innerHTML = "Hello";
+    }, "1000");
+}
+
+    
+);
+
+/*
 function ContactValidation() {
 
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -26,6 +47,29 @@ function ContactValidation() {
     } else {
         msg.innerHTML = "Email Erroné";
     }
-  
 }
-ContactValidation();
+ContactValidation();*/
+
+document.getElementById('email').addEventListener('change', event => {
+    if (event.target.value = '') {
+        document.getElementById('msg').style.display = 'none';
+    } else {
+        document.getElementById('msg').style.display = 'flex';
+    }
+})
+
+document.getElementById('contact-form').addEventListener('change', event => {
+    if (
+        document.getElementById('sujet-input').value != ''
+        && document.getElementById('email-input').value != ''
+        && document.getElementById('prenom-input').value != ''
+        && document.getElementById('nom-input').value != ''
+        && document.getElementById('message-textarea').value != ''
+    ) {
+        document.getElementById('form-button').removeAttribute('disabled')
+    } else {
+        document.getElementById('form-button').setAttribute('disabled', 'true')
+    }
+})
+
+
